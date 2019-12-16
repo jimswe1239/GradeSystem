94c6a74fa8fc195814c00bfb6dd5efaeb4873f2e
richard6094 <490164814@qq.com>
2019-12-16 14:41:08 -0500

    14_40

---
+++ GradeSystem/src/UI/AddStudent.java
@@ -0,0 +1,166 @@
+package UI;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.border.EmptyBorder;
+
+import Logic.Component;
+import Logic.Course;
+import Logic.Section;
+import Logic.Student;
+
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.tree.TreePath;
+import java.awt.Color;
+
+public class AddStudent extends JFrame {
+
+	private JPanel contentPane;
+	private JTextArea textArea;
+	private JTextArea textArea_1;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+//					AddComponent frame = new AddComponent();
+//					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public AddStudent(Course course, MainWindow mw) {
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 504, 319);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 0, 490, 194);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblAddComponent = new JLabel("Add a Student");
+		//lblAddComponent.setFont(new Font("Yu Gothic UI Semibold", Font.PLAIN, 14));
+		lblAddComponent.setBounds(10, 10, 134, 21);
+		panel.add(lblAddComponent);
+		
+		textArea = new JTextArea();
+		textArea.setBounds(168, 41, 129, 21);
+		panel.add(textArea);
+		textArea.setColumns(10);
+		
+		JLabel lblComponentName = new JLabel("First Name");
+		//lblComponentName.setFont(new Font("Yu Gothic UI Light", Font.PLAIN, 12));
+		lblComponentName.setBounds(40, 43, 104, 15);
+		panel.add(lblComponentName);
+		
+		textArea_1 = new JTextArea();
+		textArea_1.setColumns(10);
+		textArea_1.setBounds(168, 108, 129, 21);
+		panel.add(textArea_1);
+		
+		JLabel lblScalepercentage = new JLabel("Section");
+		//lblScalepercentage.setFont(new Font("Yu Gothic UI Light", Font.PLAIN, 12));
+		lblScalepercentage.setBounds(40, 110, 104, 15);
+		panel.add(lblScalepercentage);
+		
+		JLabel lblLastName = new JLabel("Last Name");
+		lblLastName.setBounds(40, 74, 104, 15);
+		panel.add(lblLastName);
+		
+		JTextArea textArea_2 = new JTextArea();
+		textArea_2.setColumns(10);
+		textArea_2.setBounds(168, 72, 129, 21);
+		panel.add(textArea_2);
+		
+		JButton btnNewButton = new JButton("OK");
+		btnNewButton.setFont(new Font("Verdana", Font.PLAIN, 12));
+		btnNewButton.setBounds(73, 219, 97, 23);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("cancel");
+		btnNewButton_1.setFont(new Font("Verdana", Font.PLAIN, 12));
+		btnNewButton_1.setBounds(289, 219, 97, 23);
+		contentPane.add(btnNewButton_1);
+		
+		btnNewButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				String firstName = textArea.getText();
+				String lastName = textArea_2.getText();
+				String studentSectionStr = textArea_1.getText();
+				int sectionNum;
+				Section s;
+				
+				try {
+					sectionNum = Integer.parseInt(studentSectionStr);
+				}
+				catch(NumberFormatException e1) {
+					JOptionPane.showMessageDialog(btnNewButton,"Incorrect format about the Section!");
+					return;
+				}
+				//if OK
+				//check if the section exists
+				if((s=course.findSection(studentSectionStr))==null) {
+					course.setSectionsTo(sectionNum);
+					s=course.findSection(studentSectionStr);
+				}
+				//add students
+				s.addStudent(new Student(firstName, lastName));
+				JOptionPane.showMessageDialog(btnNewButton,"Add new student: "+firstName+" "+lastName+" Succesfully!");
+				mw.refreshTableNStatistic();
+				
+				
+//				Component newComponent = new Component(componentName);
+//				if(root.isComponentNameExist(newComponent)==true) {
+//					JOptionPane.showMessageDialog(btnNewButton,"Name exists!");
+//					return;
+//				}
+//				root.addComponentAndScale(newComponent, percentage);
+//				node.add(new GSComponentNode(newComponent));
+//				JOptionPane.showMessageDialog(btnNewButton,"Add new component: "+componentName+" Succesfully!");
+				
+				AddStudent.this.setVisible(false);
+				mw.setModified();
+				dispose();
+			}
+			
+		});
+		
+		btnNewButton_1.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				AddStudent.this.setVisible(false);
+				dispose();
+			}
+		});
+	}
+}
